a41a4e065f59fc8760589ea489708e2e8740841e
Dre mf Randaci <drerandaci@gmail.com>
2017-11-24 00:52:37 -0500

    styles to navbar

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -4,23 +4,23 @@ app.run(function(FIREBASE_CONFIG){
     firebase.initializeApp(FIREBASE_CONFIG);    
 });
 
-app.config(function($routeProvider){
-    $routeProvider
-    .when( "/about", {
-        templateUrl: 'partials/about.html',
-        controller: 'AboutCtrl'
-    })
-    .when( "/projects", {
-        templateUrl: 'partials/projects.html',
-        controller: 'ProjectsCtrl'
-    })
-    .when( "/blog", {
-        templateUrl: 'partials/blog.html',
-        controller: 'BlogCtrl'
-    })
-    .when( "/main-view", {
-        templateUrl: 'partials/main-view.html',
-        controller: 'MainViewCtrl'
-    })
-    .otherwise('/main-view');
-});
+// app.config(function($routeProvider){
+//     $routeProvider
+//     .when( "/about", {
+//         templateUrl: 'partials/about.html',
+//         controller: 'AboutCtrl'
+//     })
+//     .when( "/projects", {
+//         templateUrl: 'partials/projects.html',
+//         controller: 'ProjectsCtrl'
+//     })
+//     .when( "/blog", {
+//         templateUrl: 'partials/blog.html',
+//         controller: 'BlogCtrl'
+//     })
+//     .when( "/main-view", {
+//         templateUrl: 'partials/main-view.html',
+//         controller: 'MainViewCtrl'
+//     })
+//     .otherwise('/main-view');
+// });
