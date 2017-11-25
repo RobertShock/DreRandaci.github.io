7d01e81fef3eb4c15f337a12fc08582a0e804155
Dre mf Randaci <drerandaci@gmail.com>
2017-11-25 01:52:21 -0500

    layout

--- javascripts/controllers/BlogCtrl.js
+++ javascripts/controllers/BlogCtrl.js
@@ -4,7 +4,6 @@ app.controller('BlogCtrl', function( $scope,  FirebaseService ){
     const getBlogs = () => {
         FirebaseService.getAllBlogs().then((results) => {            
             $scope.blogs = results; 
-            console.log(results);
         }).catch((err) => {
             console.log('error in getAllBlogs:', err);
         });
--- javascripts/controllers/ProjectsCtrl.js
+++ javascripts/controllers/ProjectsCtrl.js
@@ -5,7 +5,6 @@ app.controller('ProjectsCtrl', function( $scope, FirebaseService ){
     const getProjects = () => {
         FirebaseService.getAllProjects().then((results) => {            
             $scope.projects = results; 
-            console.log(results);
         }).catch((err) => {
             console.log('error in getAllProjects:', err);
         });
