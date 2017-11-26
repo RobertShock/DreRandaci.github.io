cbdaa643117bd5aad1faeae05bd2614758afdce9
Dre Randaci <drerandaci@gmail.com>
2017-11-26 14:26:57 -0600

    Update AppConstants.js

--- javascripts/AppConstants.js
+++ javascripts/AppConstants.js
@@ -1,10 +1,10 @@
 'use strict';
 
 app.constant("FIREBASE_CONFIG", {
-    apiKey: "AIzaSyAoY1fqRa8knq60w5bG_NKM-6GHCbJ6Tw8",
+    apiKey: "",
     authDomain: "dre-randaci-personal.firebaseapp.com",
     databaseURL: "https://dre-randaci-personal.firebaseio.com",
     projectId: "dre-randaci-personal",
     storageBucket: "dre-randaci-personal.appspot.com",
-    messagingSenderId: "94870622426"
+    messagingSenderId: ""
   });
